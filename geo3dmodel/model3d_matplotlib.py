fc783b450fb9fc222568ee0ec75879b476257d0d
rgeologist <raymi.castilla@geologist.com>
2026-08-31 10:35:47 +0200

    Fully implement plotting methods in model3d_matplotlib.py

--- geo3dmodel/model3d_matplotlib.py
+++ geo3dmodel/model3d_matplotlib.py
@@ -6,11 +6,13 @@ Created on Wed May 12 13:39:08 2021
 """
 from __future__ import annotations
 import numpy as np
+import pandas as pd
 from typing import Any, Dict, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 
 from .model3d_abstract import Model3D_abstract
+from . import model3d as m3d
 
 class Model3D_matplotlib(Model3D_abstract):
     """3D Model representation using Matplotlib.
@@ -36,7 +38,13 @@ class Model3D_matplotlib(Model3D_abstract):
             borehole (Any): The borehole object to plot.
             **kwargs: Additional plotting arguments.
         """
-        pass
+        survey_name = kwargs.pop('survey_name', 'preferred')
+        try:
+            df = borehole.surveys[survey_name].data        
+            x, y, z = df.loc[:, ['x', 'y', 'z']].to_numpy().T        
+            self.ax.plot(x, y, z, **kwargs)
+        except AttributeError:
+            pass
     
     def plot_rectangular_mesh(
         self,
@@ -69,7 +77,18 @@ class Model3D_matplotlib(Model3D_abstract):
             col (int, optional): Subplot column index. Defaults to 1.
             **kwargs: Additional plotting arguments.
         """
-        pass
+        if None not in (strike, dip):
+            nodes_out, triangles_out = m3d.build_rectangular_mesh(
+                strike=strike, dip=dip, center=center,
+                i_elements=i_elements, i_length=i_length,
+                j_elements=j_elements, j_length=j_length
+            )
+            nodes = nodes_out
+            triangles = triangles_out
+        
+        if nodes is not None and triangles is not None:
+            x, y, z = nodes.T
+            self.ax.plot_trisurf(x, y, z, triangles=triangles, **kwargs)
     
     def plot_plane(
         self,
@@ -86,7 +105,15 @@ class Model3D_matplotlib(Model3D_abstract):
             trace_kwargs (dict, optional): Trace arguments. Defaults to None.
             **kwargs: Additional plotting arguments.
         """
-        pass
+        if c_hull_kwargs is None:
+            c_hull_kwargs = {}
+        if trace_kwargs is None:
+            trace_kwargs = {}
+            
+        hull3D = points_3d_df.pcloud.convex_hull_3D(**c_hull_kwargs)
+        
+        x, y, z = hull3D[:, 0], hull3D[:, 1], hull3D[:, 2]
+        self.ax.plot_trisurf(x, y, z, **trace_kwargs)
     
     def plot_point_cloud(self, df: Any, **kwargs: Any) -> None:
         """Plot a point cloud.
@@ -95,7 +122,11 @@ class Model3D_matplotlib(Model3D_abstract):
             df (Any): DataFrame containing point cloud data.
             **kwargs: Additional plotting arguments.
         """
-        pass
+        marker_dict = kwargs.pop('marker_dict', {})
+        other_trace_kwargs = kwargs.pop('other_trace_kwargs', {})
+        
+        combined_kwargs = {**marker_dict, **other_trace_kwargs, **kwargs}
+        self.ax.scatter(df.x, df.y, df.z, **combined_kwargs)
     
     def add_borehole_path(self, borehole: Any, **kwargs: Any) -> None:
         """Add the path of a borehole to the model.
@@ -104,7 +135,18 @@ class Model3D_matplotlib(Model3D_abstract):
             borehole (Any): The borehole object.
             **kwargs: Additional arguments.
         """
-        pass
+        coord_system = kwargs.pop('coord_system', 'lv95')
+        try:
+            bh_survey = borehole.surveys.preferred.data
+            wh_coords = (
+                borehole.header.easting[coord_system],
+                borehole.header.northing[coord_system],
+                borehole.header.elevation_gl.iloc[0],
+            )
+            x, y, z = (bh_survey.loc[:, ['x', 'y', 'z']] + wh_coords).values.T
+            self.ax.plot(x, y, z, label=borehole.name, **kwargs)
+        except AttributeError:
+            pass
     
     def add_boreholes_paths(
         self,
@@ -119,20 +161,51 @@ class Model3D_matplotlib(Model3D_abstract):
             names_to_skip (list, optional): List of borehole names to skip. Defaults to None.
             **kwargs: Additional arguments.
         """
-        pass
+        if names_to_skip is None:
+            names_to_skip = []
+        for bh_name, bh in borehole_dict.items():
+            if any(name in bh_name for name in names_to_skip):
+                continue
+            self.add_borehole_path(bh, **kwargs)
     
-    def add_borehole_interval(self) -> None:
+    def add_borehole_interval(
+        self,
+        survey: pd.DataFrame,
+        md_start: float,
+        md_end: float,
+        sampling: float = 0.05,
+        x_column: str = 'x',
+        y_column: str = 'y',
+        z_column: str = 'z',
+        interval_fmt_kwargs: Optional[Dict[str, Any]] = None,
+        trace_kwargs: Optional[Dict[str, Any]] = None
+    ) -> None:
         """Add a borehole interval to the model."""
-        pass
+        if interval_fmt_kwargs is None:
+            interval_fmt_kwargs = {}
+        if trace_kwargs is None:
+            trace_kwargs = {}
+            
+        md_array = np.arange(md_start, md_end, sampling)
+        x = np.interp(md_array, survey.md, survey[x_column])
+        y = np.interp(md_array, survey.md, survey[y_column])
+        z = np.interp(md_array, survey.md, survey[z_column])
+        
+        combined_kwargs = {**interval_fmt_kwargs, **trace_kwargs}
+        self.ax.plot(x, y, z, **combined_kwargs)
     
-    def plot_log_along_borehole(self, borehole: Any, log: Any) -> None:
+    def plot_log_along_borehole(self, borehole: Any, log: Any, **kwargs) -> None:
         """Plot log data along a borehole path.
 
         Args:
             borehole (Any): The borehole object.
             log (Any): The log data to plot.
         """
-        pass
+        try:
+            survey = borehole.survey.reindex(method='nearest', index=log.index)
+            self.ax.scatter(survey.x, survey.y, survey.z, c=log.values, label=f'{log.name}:{borehole.name}', **kwargs)
+        except AttributeError:
+            pass
     
     def add_one_structure_as_disk(
         self,
@@ -151,7 +224,14 @@ class Model3D_matplotlib(Model3D_abstract):
             disk_kwargs (dict, optional): Arguments for disk creation. Defaults to None.
             mesh_kwargs (dict, optional): Arguments for mesh representation. Defaults to None.
         """
-        pass
+        if disk_kwargs is None:
+            disk_kwargs = {}
+        if mesh_kwargs is None:
+            mesh_kwargs = {}
+            
+        disk, triangles = m3d.build_disk(strike, dip, center=center, **disk_kwargs)
+        
+        self.ax.plot_trisurf(disk[:, 0], disk[:, 1], disk[:, 2], triangles=triangles, **mesh_kwargs)
 
     def _set_axes_equal(self) -> None:
         """Make axes of 3D plot have equal scale."""
